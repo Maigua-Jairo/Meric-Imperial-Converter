0d44fffcec50c7979aa69e444fc420cde6c18270
Maigua-Jairo <jairo.maigua@epn.edu.ec>
2026-04-03 22:31:51 -0500

    upgrade test2

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,42 +6,59 @@ chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
 
-  test('valid input 10L', done => {
-    chai.request(server).get('/api/convert').query({input:'10L'}).end((err, res) => {
-      assert.equal(res.status, 200);
-      assert.equal(res.body.initNum, 10);
-      assert.equal(res.body.initUnit, 'L');
-      assert.approximately(res.body.returnNum, 2.64172, 0.001);
-      done();
-    });
+  test('Convert a valid input such as 10L', done => {
+    chai.request(server)
+      .get('/api/convert')
+      .query({ input: '10L' })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.body.initNum, 10);
+        assert.equal(res.body.initUnit, 'L');
+        assert.approximately(res.body.returnNum, 2.64172, 0.001);
+        assert.equal(res.body.returnUnit, 'gal');
+        done();
+      });
   });
 
- test('invalid input 32g', done => {
-  chai.request(server).get('/api/convert').query({input:'32g'}).end((err, res) => {
-    assert.equal(res.body, 'invalid unit');  
-    done();
+  test('Convert an invalid input such as 32g', done => {
+    chai.request(server)
+      .get('/api/convert')
+      .query({ input: '32g' })
+      .end((err, res) => {
+        assert.equal(res.body, 'invalid unit');
+        done();
+      });
   });
-});
 
-test('invalid number 3/7.2/4kg', done => {
-  chai.request(server).get('/api/convert').query({input:'3/7.2/4kg'}).end((err, res) => {
-    assert.equal(res.body, 'invalid number');
-    done();
+  test('Convert an invalid number such as 3/7.2/4kg', done => {
+    chai.request(server)
+      .get('/api/convert')
+      .query({ input: '3/7.2/4kg' })
+      .end((err, res) => {
+        assert.equal(res.body, 'invalid number');
+        done();
+      });
   });
-});
 
-test('invalid number and unit 3/7.2/4kilomegagram', done => {
-  chai.request(server).get('/api/convert').query({input:'3/7.2/4kilomegagram'}).end((err, res) => {
-    assert.equal(res.body, 'invalid number and unit');
-    done();
+  test('Convert an invalid number AND unit such as 3/7.2/4kilomegagram', done => {
+    chai.request(server)
+      .get('/api/convert')
+      .query({ input: '3/7.2/4kilomegagram' })
+      .end((err, res) => {
+        assert.equal(res.body, 'invalid number and unit');
+        done();
+      });
   });
-});
 
-  test('no number kg', done => {
-    chai.request(server).get('/api/convert').query({input:'kg'}).end((err, res) => {
-      assert.equal(res.body.initNum, 1);
-      assert.equal(res.body.initUnit, 'kg');
-      done();
-    });
+  test('Convert with no number such as kg', done => {
+    chai.request(server)
+      .get('/api/convert')
+      .query({ input: 'kg' })
+      .end((err, res) => {
+        assert.equal(res.body.initNum, 1);
+        assert.equal(res.body.initUnit, 'kg');
+        done();
+      });
   });
+
 });
