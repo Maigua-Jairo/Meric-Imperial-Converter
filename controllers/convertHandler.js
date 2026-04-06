1217b7dd0ac8f278730ea7319d251eed647764f6
Maigua-Jairo <jairo.maigua@epn.edu.ec>
2026-04-05 19:38:41 -0500

    error correction 2 in test 5

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -37,26 +37,28 @@ this.getUnit = function(input) {
     return result;
   };
 
-  this.convert = function (initNum, initUnit) {
-    const galToL = 3.78541;
-    const lbsToKg = 0.453592;
-    const miToKm = 1.60934;
-    var result;
+this.convert = function(initNum, initUnit) {
+  const galToL  = 3.78541;
+  const lbsToKg = 0.453592;
+  const miToKm  = 1.60934;
+  var result;
 
-    if (initUnit == 'gal' || initUnit == 'GAL') {
-      result = (initNum * galToL).toFixed(5);
-    } else if (initUnit == 'L' || initUnit == 'l') {
-      result = (initNum / galToL).toFixed(5);
-    }
-    if (initUnit === 'lbs' || initUnit === 'LBS') {
-      result = initNum * lbsToKg
-    } else if (initUnit === 'kg' || initUnit === 'KG') {
+  if (initUnit === 'gal') {
+    result = initNum * galToL;
+  } else if (initUnit === 'L') {
+    result = initNum / galToL;
+  } else if (initUnit === 'mi') {
+    result = initNum * miToKm;
+  } else if (initUnit === 'km') {
+    result = initNum / miToKm;
+  } else if (initUnit === 'lbs') {
+    result = initNum * lbsToKg;
+  } else if (initUnit === 'kg') {
+    result = initNum / lbsToKg;
+  }
 
-      result = parseFloat((initNum / lbsToKg).toFixed(5))
-    }
-    return result;
-  };
-
+  return parseFloat(result.toFixed(5));
+};
   this.getString = function (initNum, initUnit, returnNum, returnUnit) {
     return `${initNum} ${this.spellOutUnit(initUnit)} converts to ${returnNum} ${this.spellOutUnit(returnUnit)}`;
   };
