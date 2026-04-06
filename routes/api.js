3127ad675effea8d9453a3b6cdb79ed929bad276
Maigua-Jairo <jairo.maigua@epn.edu.ec>
2026-04-05 21:16:54 -0500

    test 7 updated

--- routes/api.js
+++ routes/api.js
@@ -2,31 +2,25 @@
 
 const ConvertHandler = require('../controllers/convertHandler.js');
 
-module.exports = function (app) {
+module.exports = function(app) {
   const convertHandler = new ConvertHandler();
 
   app.route('/api/convert')
-    .get(function (req, res) {
-      var input = req.query.input;
-      var initNum = convertHandler.getNum(input);
+    .get(function(req, res) {
+      var input    = req.query.input;
+      var initNum  = convertHandler.getNum(input);
       var initUnit = convertHandler.getUnit(input);
-      var returnNum = convertHandler.convert(initNum, initUnit);
+
+      if (initNum === 'invalid number' && initUnit === 'invalid unit') {
+        return res.json('invalid number and unit');
+      }
+      if (initNum === 'invalid number') return res.json('invalid number');
+      if (initUnit === 'invalid unit')  return res.json('invalid unit');
+
+      var returnNum  = convertHandler.convert(initNum, initUnit);
       var returnUnit = convertHandler.getReturnUnit(initUnit);
-      var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+      var toString   = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
 
-      const numInvalid = initNum === 'invalid number';
-      const unitInvalid = initUnit === 'invalid unit';
-
-      if (numInvalid && unitInvalid) return res.json({ error: 'invalid number and unit' });
-      if (numInvalid) return res.json({ error: 'invalid number' });
-      if (unitInvalid) return res.json({ error: 'invalid unit' });
-
-      res.json({
-        initNum: initNum,
-        initUnit: initUnit,
-        returnNum: returnNum,
-        returnUnit: returnUnit,
-        string: toString
-      });
+      res.json({ initNum, initUnit, returnNum, returnUnit, string: toString });
     });
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -23,15 +23,30 @@ suitev('Functional Tests', function () {
                 chai.request(server)
                     .get('/api/convert')
                     .query({ input: '32g' })
-                    .end(function (err, res) {
+                    .end((err, res) => {
                         assert.equal(res.body, 'invalid unit');
+                        done();
                     });
             });
+
             test('Convert 3/7.2/4kg (invalid number)', function (done) {
-                //done();
+                chai.request(server)
+                    .get('/api/convert')
+                    .query({ input: '3/7.2/4kg' })
+                    .end((err, res) => {
+                        assert.equal(res.body, 'invalid number');
+                        done();
+                    });
             });
+
             test('Convert 3/7.2/4kilomegagram (invalid number and unit)', function (done) {
-                //done();
+                chai.request(server)
+                    .get('/api/convert')
+                    .query({ input: '3/7.2/4kilomegagram' })
+                    .end((err, res) => {
+                        assert.equal(res.body, 'invalid number and unit');
+                        done();
+                    });
             });
             test('Convert kg (no number)', function (done) {
                 //done();
